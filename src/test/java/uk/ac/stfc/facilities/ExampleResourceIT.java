c13806a39492de52b8843d3d14adf24d192b9b62
Sam Sheorey <91794860+sameersheorey@users.noreply.github.com>
2025-08-15 14:20:48 +0100

    Create template project

---
+++ src/test/java/uk/ac/stfc/facilities/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package uk.ac.stfc.facilities;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
