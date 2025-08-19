0cc58377e488f333a5a8e6dd6001d96a06a33c06
Sam Sheorey <91794860+sameersheorey@users.noreply.github.com>
2025-08-19 14:17:47 +0100

    #4874 Remove openapi annotations

--- src/main/java/uk/ac/stfc/facilities/controllers/EstablishmentControllerInterface.java
+++ src/main/java/uk/ac/stfc/facilities/controllers/EstablishmentControllerInterface.java
@@ -3,13 +3,8 @@ package uk.ac.stfc.facilities.controllers;
 import uk.ac.stfc.facilities.domains.establishment.EstablishmentDTO;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
-import org.eclipse.microprofile.openapi.annotations.Operation;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
-import org.eclipse.microprofile.openapi.annotations.tags.Tag;
-
 import java.util.List;
 
-@Tag(name = "establishment")
 @Path("establishment")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -17,10 +12,11 @@ public interface EstablishmentControllerInterface {
 
     @GET
     @Path("/unverified")
-    @Operation(summary = "Get all unverified establishments",
-            description = "This endpoint is anonymous and does not require any authentication")
-    @APIResponse(responseCode = "200", description = "Establishments found")
     List<EstablishmentDTO> getUnverifiedEstablishments();
+
+
+
+
 
 
 }
