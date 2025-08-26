1cbf101e126ecd5eebdd397602cdb9877d0dd67f
Sam Sheorey <91794860+sameersheorey@users.noreply.github.com>
2025-08-26 16:44:23 +0100

    #4874 Add oldEstablishmentId field to Department

--- src/main/java/uk/ac/stfc/facilities/domains/department/Department.java
+++ src/main/java/uk/ac/stfc/facilities/domains/department/Department.java
@@ -3,6 +3,7 @@ package uk.ac.stfc.facilities.domains.department;
 public class Department {
     private Long  departmentId;
     private String departmentName;
+    private Long oldEstablishmentId;
     private Long establishmentId;
 
     public Department() {}
@@ -13,6 +14,14 @@ public class Department {
         this.establishmentId = establishmentId;
     }
 
+    public Department(Long departmentId, String departmentName, Long oldEstablishmentId, Long establishmentId) {
+        this.departmentId = departmentId;
+        this.departmentName = departmentName;
+        this.oldEstablishmentId = oldEstablishmentId;
+        this.establishmentId = establishmentId;
+    }
+
+
     public Long getDepartmentId() {return departmentId;}
 
     public void setDepartmentId(Long departmentId) {this.departmentId = departmentId;}
@@ -20,6 +29,10 @@ public class Department {
     public String getDepartmentName() {return departmentName;}
 
     public void setDepartmentName(String departmentName) {this.departmentName = departmentName;}
+
+    public Long getOldEstablishmentId() {return oldEstablishmentId;}
+
+    public void setOldEstablishmentId(Long oldEstablishmentId) {this.oldEstablishmentId = oldEstablishmentId;}
 
     public Long getEstablishmentId() {return establishmentId;}
 
