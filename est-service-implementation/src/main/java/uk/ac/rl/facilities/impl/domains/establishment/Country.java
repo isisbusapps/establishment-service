fb23c854adc8cfd41e3e1ea7addfaca2368a807d
Sameer Sheorey <sameer.sheorey@stfc.co.uk>
2025-09-24 16:17:01 +0100

    Add getters and setters to country

--- est-service-implementation/src/main/java/uk/ac/rl/facilities/impl/domains/establishment/Country.java
+++ est-service-implementation/src/main/java/uk/ac/rl/facilities/impl/domains/establishment/Country.java
@@ -22,4 +22,12 @@ public class Country {
         this.countryId = countryId;
         this.countryName = countryName;
     }
+
+    public Long getCountryId() {return countryId;}
+
+    public void setCountryId(Long countryId) {this.countryId = countryId;}
+
+    public String getCountryName() {return countryName;}
+
+    public void setCountryName(String countryName) {this.countryName = countryName;}
 }
