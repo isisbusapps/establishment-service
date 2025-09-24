03feb258c8e5c4f7cbadffb1df189e6ba988052d
Sameer Sheorey <sameer.sheorey@stfc.co.uk>
2025-09-24 16:22:50 +0100

    Add application scoped annotation

--- est-service-implementation/src/main/java/uk/ac/rl/facilities/impl/domains/establishment/CountryRepositoryImpl.java
+++ est-service-implementation/src/main/java/uk/ac/rl/facilities/impl/domains/establishment/CountryRepositoryImpl.java
@@ -1,4 +1,7 @@
 package uk.ac.rl.facilities.impl.domains.establishment;
 
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class CountryRepositoryImpl implements CountryRepository {
 }
