c50994780ab66102b513a2fcdaccf87cb52470fe
Sameer Sheorey <sameer.sheorey@stfc.co.uk>
2025-09-24 16:20:58 +0100

    Fix type error

--- est-service-implementation/src/main/java/uk/ac/rl/facilities/impl/domains/establishment/CountryRepository.java
+++ est-service-implementation/src/main/java/uk/ac/rl/facilities/impl/domains/establishment/CountryRepository.java
@@ -2,5 +2,5 @@ package uk.ac.rl.facilities.impl.domains.establishment;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
-public interface CountryRepository extends PanacheRepository<Category> {
+public interface CountryRepository extends PanacheRepository<Country> {
 }
