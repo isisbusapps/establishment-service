782507b2c09a2bc56ed3de890657a3dae9b71614
Sam Sheorey <91794860+sameersheorey@users.noreply.github.com>
2025-10-15 11:01:04 +0100

    #14 implement LabelKeywordRepository

---
+++ est-service-implementation/src/main/java/uk/ac/rl/facilities/impl/domains/department/LabelKeywordRepositoryImpl.java
@@ -0,0 +1,9 @@
+package uk.ac.rl.facilities.impl.domains.department;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.List;
+
+@ApplicationScoped
+public class LabelKeywordRepositoryImpl implements LabelKeywordRepository {
+}
