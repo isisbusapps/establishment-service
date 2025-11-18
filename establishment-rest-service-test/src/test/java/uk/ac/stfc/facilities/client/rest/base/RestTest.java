2113ff24321d43537a6fa7cf12927a2696ed2a34
Sam Sheorey <91794860+sameersheorey@users.noreply.github.com>
2025-11-18 17:11:15 +0000

    #25 Remove unused permissions

--- establishment-rest-service-test/src/test/java/uk/ac/stfc/facilities/client/rest/base/RestTest.java
+++ establishment-rest-service-test/src/test/java/uk/ac/stfc/facilities/client/rest/base/RestTest.java
@@ -5,8 +5,6 @@ import io.restassured.http.Header;
 public abstract class RestTest extends RestTestDatabaseClient {
     public static String BASE_URI_PROPERTY = "userOfficeTest.restUri";
     private static final AuthConfig authConfig = new AuthConfig();
-    protected final static Header AUTH_SUPER_ADMIN_HEADER = new Header("Authorization", authConfig.getSuperAdminToken());
-    protected final static Header AUTH_ADMIN_HEADER = new Header("Authorization", authConfig.getAdminToken());
     protected final static Header AUTH_USER_OFFICE_HEADER = new Header("Authorization", authConfig.getUserOfficeToken());
     protected final static Header AUTH_USER_HEADER = new Header("Authorization", authConfig.getUserToken());
 
